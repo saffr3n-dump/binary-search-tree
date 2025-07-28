ed7e80be50df554eb949a77d279be155e0a9d0e8
saffr3n <saffr3n@gmail.com>
2025-07-28 22:36:04 +0300

    feat: add Tree.insert() method

--- index.js
+++ index.js
@@ -3,6 +3,23 @@ class Tree {
     this.root = this.#buildTree(values);
   }
 
+  insert(value) {
+    let root = this.root;
+    if (!root) {
+      this.root = new Node(value);
+      return;
+    }
+    while (true) {
+      if (value === root.value) return;
+      const path = value < root.value ? 'left' : 'right';
+      if (!root[path]) {
+        root[path] = new Node(value, root);
+        return;
+      }
+      root = root[path];
+    }
+  }
+
   #buildTree(values) {
     const norm = this.#removeDupes(values).sort((a, b) => a - b);
     return this.#buildTreeRec(norm, 0, norm.length);
